ba5b854e969351d0c0004d4352a3c38ea181b09c
Alpha C137 <alphasydwell93@gmail.com>
2025-07-14 00:40:45 +0000

    feat: update Sentry initialization with DSN and enable console logging integration

--- server/instrument.js
+++ server/instrument.js
@@ -1,9 +1,13 @@
 const Sentry = require("@sentry/node");
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN || "",
+  dsn: "https://b1a09c8f55d8b6af33abf1d54af4a95e@o4509663941361664.ingest.de.sentry.io/4509663944769616",
   sendDefaultPii: true,
   tracesSampleRate: 1.0,
+  _experiments: { enableLogs: true },
+  integrations: [
+    Sentry.consoleLoggingIntegration({ levels: ["log", "error", "warn"] })
+  ]
 });
 
 module.exports = Sentry;
